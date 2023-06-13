f4bff9b2a3eae92070b0d3d81b9593886dfb2e11
Manuimfeld <manu_jadir@hotmail.com>
2023-06-13 15:52:09 -0300

    Add push notifications

--- webpush.js
+++ webpush.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const webpush = require("web-push");
-const { PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY } = process.env;
+const PUBLIC_VAPID_KEY = process.env.PUBLIC_VAPID_KEY;
+const PRIVATE_VAPID_KEY = process.env.PRIVATE_VAPID_KEY;
 
 webpush.setVapidDetails(
   "mailto:rifas_achinelli",
